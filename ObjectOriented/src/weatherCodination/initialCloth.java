68ebe19d035e5c175f43091e679bd11378d20255
지우 <@DESKTOP-86CSTSF>
2017-11-30 01:31:10 +0900

    남자 옷 코디 추가

--- ObjectOriented/src/weatherCodination/initialCloth.java
+++ ObjectOriented/src/weatherCodination/initialCloth.java
@@ -45,18 +45,39 @@ public class initialCloth {
 		String[] w_initialOuter= {"cardigan1.PNG", "coat1.PNG","coat2.PNG","coat3.PNG","coat4.PNG",
 								"coat5.PNG","pd1.PNG","pd2.PNG","pd3.PNG","pd4.PNG",
 								"w_outer1.PNG","w_outer2.PNG","w_outer3.PNG","w_outer0.PNG"};
+		String[] m_initialOuter= {"outer1.PNG","outer2.PNG","outer3.PNG","outer4.PNG","outer5.PNG",
+								"w_outer1.PNG","w_outer2.PNG","w_outer3.PNG","w_outer4.PNG"};
+		
 		String[] w_link= {"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/item/7767169","https://codibook.net/item/7786366",
-				"https://ko.codibook.net/item/7786180","https://codibook.net/item/7780295?recStrategy=004061","https://codibook.net/codi/7502288","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/"};
+						"https://ko.codibook.net/item/7786180","https://codibook.net/item/7780295?recStrategy=004061","https://codibook.net/codi/7502288","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/"};
+		String[] m_link= {"http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/",
+						"http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/"};
 		
 		int[] w_thick={1,3,3,3,4,3,5,5,3,2,3,3,3,5};
+		int[] m_thick= {5,5,5,3,5,5,3,3,5};
+		
 		int[][] w_style= {{3,0,1,3},{5,0,2,2},{5,0,2,4},{5,0,5,2},{0,5,0,5},
 						{2,3,0,5},{3,3,0,3},{0,5,0,3},{3,4,0,3},{2,2,0,5},
 						{0,5,0,2},{5,1,3,2},{1,4,0,5},{5,5,5,5}};
+		int[][] m_style= {{5,0,5,1},{5,3,0,3},{5,3,5,5},{5,5,0,5},{5,0,5,0},
+						{5,5,5,5},{2,5,0,5},{5,5,0,5},{5,0,3,3}};
+		
 		int[] w_weather= {2,2,2,2,2,2,2,2,2,2,1,1,1,1};
+		int[] m_weather= {2,2,2,2,2,1,1,1,1,1};
 		
 		
 		if(sex=="남자") {			
+			clo_outer.clear();
 			outerPath=imgPath + "outer/man/";
+			
+			for (int i = 0; i < m_initialOuter.length; i++) {
+				ImageIcon icon = new ImageIcon(outerPath + m_initialOuter[i]);
+				Image img = icon.getImage();
+				Image changedImg = img.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+
+				outer tmp = new outer(m_weather[i], m_thick[i], m_style[i], changedImg, m_link[i]);
+				clo_outer.add(tmp);
+			}
 		}else {
 			outerPath=imgPath + "outer/";
 			for (int i = 0; i < w_initialOuter.length; i++) {
@@ -75,39 +96,62 @@ public class initialCloth {
 	 * */
 	public void initialTop(String sex)  {
 		String topPath;
-		String[] initialtop= {"mtm1.PNG", "nt1.PNG","nt2.PNG","nt3.PNG","nt4.PNG",
+		
+		String[] w_initialtop= {"mtm1.PNG", "nt1.PNG","nt2.PNG","nt3.PNG","nt4.PNG",
 								"nt5.PNG","nt6.PNG","nt6.PNG","nt8.PNG","shirts1.PNG",
 								"shirts2.PNG","shirts3.PNG","t1.PNG","w_top1.PNG","w_top2.PNG",
 								"w_top3.PNG","w_top4.PNG","w_top5.PNG","w_top6.PNG","w_top7.PNG"};
+		String[] m_initialTop= {"top1.PNG","top2.PNG","top3.PNG","top4.PNG","top5.PNG",
+				"top6.PNG","w_top1.PNG","w_top2.PNG","w_top3.PNG","w_top4.PNG",
+				"w_top5.PNG","w_top6.PNG","w_top7.PNG"};
 		
-		int[] thick={3,4,4,5,5,
+		int[] w_thick={3,4,4,5,5,
 					3,5,4,4,2,
 					2,2,2,5,3,
 					4,2,3,1,2};
-		int[][] style= {{3,3,0,3},{3,3,3,2},{5,0,5,3},{2,3,0,3},{3,3,0,4},
+		int[] m_thick={3,3,5,5,2,2,
+				5,5,5,5,5,3,3};
+		int[][] w_style= {{3,3,0,3},{3,3,3,2},{5,0,5,3},{2,3,0,3},{3,3,0,4},
 						{0,5,0,0},{1,5,0,0},{3,3,3,2},{0,2,0,5},{5,0,5,5},
 						{5,0,3,3},{3,0,0,5},{3,3,3,3},{3,2,5,3},{1,4,2,1},
 						{5,5,1,2},{1,3,0,4},{5,1,5,2},{0,5,1,1},{5,0,2,2}};
+		int[][] m_style= {{5,2,3,3},{5,5,0,2},{5,2,5,2},{5,3,5,2},{5,3,3,2},
+						{5,5,5,3},{5,5,5,3},{5,5,5,3},{5,5,1,3},{5,5,3,3},
+						{5,2,5,3},{5,3,5,3},{5,3,5,3}};
 		
-		String[] link= {"https://codibook.net/item/7780475?recStrategy=004061","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/",
+		String[] w_link= {"https://codibook.net/item/7780475?recStrategy=004061","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/",
 				"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/",
 				"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/"};
-		int[] weather= {2,2,2,2,2,
+		String[] m_link= {"http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/"};
+		
+		int[] w_weather= {2,2,2,2,2,
 						2,2,2,2,2,
 						2,2,2,1,1,
 						1,1,1,1,1};
+		int[] m_weather= {2,2,2,2,2,2,1,1,1,1,1,1,1};
 
-		if(sex=="남자") {			
-			topPath=imgPath + "outer/man/";
-		}else {
-			topPath=imgPath+"top/";
+		if(sex=="남자") {
+			clo_top.clear();
+			topPath=imgPath + "top/man/";
 			
-			for (int i = 0; i < initialtop.length; i++) {
-				ImageIcon icon = new ImageIcon(topPath + initialtop[i]);
+			for (int i = 0; i < m_initialTop.length; i++) {
+				ImageIcon icon = new ImageIcon(topPath + m_initialTop[i]);
 				Image img = icon.getImage();
 				Image changedImg = img.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
 
-				top tmp = new top(weather[i], thick[i], style[i], changedImg, link[i]);
+				top tmp = new top(m_weather[i], m_thick[i], m_style[i], changedImg, m_link[i]);
+				clo_top.add(tmp);
+			}
+			
+		}else {
+			topPath=imgPath+"top/";
+			
+			for (int i = 0; i < w_initialtop.length; i++) {
+				ImageIcon icon = new ImageIcon(topPath + w_initialtop[i]);
+				Image img = icon.getImage();
+				Image changedImg = img.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+
+				top tmp = new top(w_weather[i], w_thick[i], w_style[i], changedImg, w_link[i]);
 				clo_top.add(tmp);
 			}
 			
@@ -118,29 +162,47 @@ public class initialCloth {
 	 * */
 	public void initialPants(String sex)  {
 		String pantsPath;
+		
 		String[] initialPants= {"pants1.PNG","pants2.PNG","pants3.PNG","pants4.PNG","pants5.PNG",
 				"skt1.PNG","skt2.PNG","skt3.PNG","skt4.PNG","skt5.PNG",
 				"w_pants1.PNG","w_pants2.PNG","w_pants3.PNG","w_pants4.PNG","w_pants5.PNG",
 				"w_pants6.PNG","w_pants7.PNG","w_pants8.PNG","w_pants9.PNG","w_pants10.PNG"};
+		String[] m_initialPants= {"pants1.PNG","pants2.PNG","pants3.PNG","pants4.PNG","pants5.PNG",
+				"w_pants1.PNG","w_pants2.PNG","w_pants3.PNG","w_pants4.PNG","w_pants5.PNG"};
 		
 		String[] link= {"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/item/7774804","https://codibook.net/item/7639641",
 				"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/"};
+		String[] m_link= {"http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/"};
+		
 		
 		int[] thick={3,3,3,3,3,
 					4,4,4,4,3,
 					4,4,5,3,2,
 					2,2,2,2,2};
+		int[] m_thick= {3,3,3,3,3,3,3,3,3,3};
+		
 		int[][] style= {{5,2,3,3},{3,2,3,4},{0,0,0,5},{3,2,0,5},{5,2,2,3},
 						{2,5,5,0},{2,0,0,5},{4,0,2,3},{0,5,3,5},{5,0,4,1},
 						{4,3,5,1},{1,5,3,0},{3,3,3,3},{2,2,0,2},{3,5,3,0},
 						{2,4,4,0},{5,2,4,3},{5,2,2,2},{2,0,0,5},{5,0,1,2}};
-		int[] weather= {2,2,2,2,2,
-						2,2,2,2,2,
-						1,1,1,1,1,
-						1,1,1,1,1};
+		int[][] m_style= {{3,3,3,3},{3,3,3,3},{5,3,5,3},{3,3,3,3},{3,3,3,3},
+						{3,3,3,3},{3,3,3,3},{3,3,3,3},{3,3,3,3},{3,3,3,3}};
+		int[] weather= {2,2,2,2,2,2,2,2,2,2,
+						1,1,1,1,1,1,1,1,1,1};
+		int[] m_weather= {2,2,2,2,2,1,1,1,1,1};
 		
 		if(sex=="남자") {			
-			pantsPath=imgPath + "outer/man/";
+			clo_pants.clear();
+			pantsPath=imgPath + "pants/man/";
+			
+			for (int i = 0; i < m_initialPants.length; i++) {
+				ImageIcon icon = new ImageIcon(pantsPath + m_initialPants[i]);
+				Image img = icon.getImage();
+				Image changedImg = img.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+
+				pants tmp = new pants(m_weather[i], m_thick[i], m_style[i], changedImg, m_link[i]);
+				clo_pants.add(tmp);
+			}
 		}else {
 			pantsPath=imgPath+"pants/";
 			
@@ -162,16 +224,34 @@ public class initialCloth {
 		String shoesPath;
 		String[] initialShoes= {"sh1.PNG","sh2.PNG","sh3.PNG","sh4.PNG","sh5.PNG","sh6.PNG",
 								"w_shoes1.PNG","w_shoes2.PNG","w_shoes3.PNG","w_shoes4.PNG","w_shoes5.PNG","w_shoes6.PNG","w_shoes7.PNG"};
+		String[] m_initialShoes= {"shoes1.PNG","shoes2.PNG","shoes3.PNG","shoes4.PNG","shoes5.PNG","w_shoes1.PNG","w_shoes2.PNG","w_shoes3.PNG","w_shoes4.PNG"};
 		
 		int[] thick={2,3,4,3,5,3,3,3,4,3,3,5,3};
+		int[] m_thick= {3,3,3,5,3,5,5,3,3};
+		
 		int[][] style= {{1,5,3,2},{5,1,2,5},{5,0,0,5},{5,5,0,5},{3,3,3,2},
 						{4,4,1,4},{5,5,0,5},{5,5,0,5},{0,3,3,0},{5,5,0,5},{5,5,0,5},{5,2,0,5},{5,0,5,0}};
+		int[][] m_style= {{3,1,5,1},{3,3,2,3},{3,3,3,3},{3,1,3,3},{3,3,3,3},{3,3,1,3},{3,3,1,3},{3,3,3,3},{3,3,3,3}};
 		
 		String[] link={"https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/","https://codibook.net/"};
+		String[] m_link= {"http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/","http://store.musinsa.com/"};
+		
 		int[] weather= {2,2,2,2,2,2,1,1,1,1,1,1,1};
+		int[] m_weather= {2,2,2,2,2,1,1,1,1};
 
-		if(sex=="남자") {			
+		if(sex=="남자") {		
+			clo_shoes.clear();
 			shoesPath=imgPath + "outer/man/";
+			
+			for (int i = 0; i < m_initialShoes.length; i++) {
+				ImageIcon icon = new ImageIcon(shoesPath + m_initialShoes[i]);
+				Image img = icon.getImage();
+				Image changedImg = img.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+
+				shoes tmp = new shoes(m_weather[i], m_thick[i], m_style[i], changedImg, m_link[i]);
+				clo_shoes.add(tmp);
+			}
+			
 		}else {
 			shoesPath = imgPath + "shoes/";
 			for (int i = 0; i < initialShoes.length; i++) {
